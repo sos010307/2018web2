4b2247c5b1b7092fc999b235e8a83d47c77298b5
sos010307 <sos010307@naver.com>
2018-04-13 17:14:53 +0900

    slide까지 완성

--- school/js/script.js
+++ school/js/script.js
@@ -1,6 +1,14 @@
 var img = $(".slider");
 var cnt = 0;
-
+var max = img.length-1;
 function slide(){
-    $()
+    $(img[cnt]).siblings().css({left:"100%"});
+    $(img[cnt]).animate({left:"-100%"},function(){
+      $(this).css({left:"100%"})
+    })
+    console.log(cnt);
+    cnt++;
+    if(cnt>max)cnt=0;
+    $(img[cnt]).animate({left:"0"});
 }
+setInterval(slide,1500);
